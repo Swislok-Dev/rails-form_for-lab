7c47a9f2e66be6418546bd8b9367d42a79414d07
Swislok-Dev <cmarker4@gmail.com>
2021-03-14 20:58:10 -0500

    commit tree 7fac07dbe604e5fd4769df899838a069f6a7c3d9

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,6 +4,7 @@ class StudentsController < ApplicationController
   end
 
   def new
+    @student = Student.new
   end
 
   def show
@@ -11,7 +12,7 @@ class StudentsController < ApplicationController
   end
 
   def create
-    @student = Student.create(params.require(:student))
+    @student = Student.create(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
@@ -21,7 +22,13 @@ class StudentsController < ApplicationController
 
   def update
     @student = Student.find(params[:id])
-    @student.update(params.require(:student))
+    @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
+  end
+
+  private
+  
+  def student_params(*args)
+    params.require(:student).permit(*args)
   end
 end
